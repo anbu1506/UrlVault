57de6e73fca032e9cbc3cc789c3b0f209996bf84
anbu1506 <arasu15062004@gmail.com>
2023-07-18 20:17:49 +0530

    GUI Stage

--- app/src/main/java/com/hunter/urlvault/MainActivity.kt
+++ app/src/main/java/com/hunter/urlvault/MainActivity.kt
@@ -3,17 +3,13 @@ package com.hunter.urlvault
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,22 +17,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.hunter.urlvault.fileSystem.Database
 import com.hunter.urlvault.fileSystem.FileSystem
 import com.hunter.urlvault.ui.theme.UrlVaultTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val db = Database(this).writableDatabase
-//        val fs = FileSystem(db)
+        val db = Database(this).writableDatabase
+        val fs = FileSystem(db)
 //        val tests = Tests()
 //        tests.execTests(fs)
-//        val childList = fs.listDir("root")
+        val childList = fs.listDir("root")
         setContent {
             UrlVaultTheme {
                 // A surface container using the 'background' color from the theme
@@ -44,41 +40,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DirView(dir = FileSystem.Dir(
-                        1,
-                        "root",
-                        1,
-                        1,
-                        mapOf(1 to "/Dir")
-                    )
-                    )
+                    DirView(childList)
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DirView(dir:FileSystem.Dir) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp)
-            .background(Color.LightGray)) {
-            Icon(Icons.Filled.Email, contentDescription = "Folder Icon", Modifier.size(50.dp).align(Alignment.CenterVertically))
-            Text(text = dir.FsName,Modifier.align(Alignment.CenterVertically).padding(10.dp))
+fun DirView(map:Map<Int,String>?){
+        LazyColumn{
+            map?.forEach { (key, value) ->
+                item {
+                    ElevatedCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+
+                    ) {
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)) {
+                            Icon(painter = painterResource(id = R.drawable.dir_icon), contentDescription = null)
+                            Text(
+                                text = value,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(48.dp)
+                                    .padding(15.dp),
+                                color = MaterialTheme.colorScheme.onBackground,
+                            )
+                        }
+
+                        }
+                    }
+                }
+            }
         }
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UrlVaultTheme {
-       DirView(dir = FileSystem.Dir(
-           1,
-           "root",
-           1,
-           1,
-           mapOf(1 to "/Dir")
-       ))
-    }
-}
--- app/src/main/java/com/hunter/urlvault/components/components.kt
+++ app/src/main/java/com/hunter/urlvault/components/components.kt
@@ -1,5 +1,3 @@
 package com.hunter.urlvault.components
 
 
-
-
